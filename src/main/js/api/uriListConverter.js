7b248d7983d073f414c4d3c687d6c54019f1a7ca
mohitsharma-r <mohitsharma.r@flipkart.com>
2020-04-19 01:36:42 +0530

    New Changes

--- src/main/js/api/uriListConverter.js
+++ src/main/js/api/uriListConverter.js
@@ -1,13 +1,11 @@
 define(function () {
     'use strict';
 
-    /* Convert a single or array of resources into "URI1\nURI2\nURI3..." */
     return {
         read: function (str /*, opts */) {
             return str.split('\n');
         },
-        write: function (obj /*, opts */) {
-            // If this is an Array, extract the self URI and then join using a newline
+        write: function (obj ) {
             if (obj instanceof Array) {
                 return obj.map(resource => resource._links.self.href).join('\n');
             } else { // otherwise, just return the self URI
